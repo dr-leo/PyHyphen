27aede42dcd838ba4935bd4e3f09f4370244290d
Dr. Leo <fhaxbox66@gmail.com>
2017-08-28 20:35:15 +0200

    Always compile C extension. Do not use Windows binaries.

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 # setup.py for the PyHyphen hyphenation package
 # (c) Dr. Leo (fhaxbox66 <at> gmail >dot< com)
 
-import os
-import platform
-import sys
 from setuptools import setup, Extension
 
 
@@ -31,7 +28,6 @@ arg_dict = dict(
         'Topic :: Text Processing :: Linguistic'
     ],
     packages=['hyphen', 'textwrap2'],
-    zip_safe=False,
     ext_modules=[
         Extension('hyphen.hnj', ['src/hnjmodule.c',
                                  'src/hyphen.c',
@@ -40,30 +36,5 @@ arg_dict = dict(
     install_requires=['appdirs', 'six'],
 )
 
-
-# Check for a binary shipping with this distribution and use it instead of compiling
-# the C sources, unless --force_build_ext is given.
-if '--force_build_ext' in sys.argv:
-    sys.argv.remove('--force_build_ext')
-else:
-    # construct string describing platform
-    if platform.system() == 'Windows':
-        if sys.maxsize > 2**32:
-            platform_descr = 'amd64'
-        else:
-            platform_descr = 'win32'
-    else:
-        # TODO this does not work on Linux: binary is recompiled from scratch
-        platform_descr = platform.system()
-
-    bin_file = os.path.join(
-        'bin', 'hnj' + '.' + platform_descr + '-' + sys.version[:3] + '.pyd')
-    print(bin_file)
-    if os.path.exists(bin_file):
-        print("""Found a suitable binary version of the C extension module.
-        This binary will be installed rather than building it from source.
-        However, if you prefer compiling, reenter 'python setup.py <command> --force_build_ext'.""")
-        arg_dict['package_data'] = {'hyphen': [bin_file]}
-        arg_dict.pop('ext_modules')  # don't try to build ext modules
 
 setup(**arg_dict)
